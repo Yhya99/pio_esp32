a4cdec1678126926455fc242a351b6cee655e3cf
Yehia Shalaby <yhyaayman.ya@gmail.com>
2026-03-19 20:38:10 +0200

    add some test cases and make a code that's make the test pass

--- lib/cbuffer/cbuffer.c
+++ lib/cbuffer/cbuffer.c
@@ -1,25 +1,41 @@
 #include "cbuffer.h"
-
+#include <stdlib.h>
 void cbuffer_init(cbuffer_t* buf) {
-
+buf->head = 0;
+buf->tail = 0;
 }
 
 bool cbuffer_empty(cbuffer_t* buf) {
-    return true;
+    return (buf->head == buf->tail);
 }
 
 void cbuffer_add(cbuffer_t* buf, uint8_t item) {
-
+    if (cbuffer_full(buf) == true)
+    {
+        buf->tail = (buf->tail + 1) & BUFFER_MASK;
+    }
+    
+    buf->buffer[buf->head] = item; // write the item
+    buf->head = (buf->head + 1) & BUFFER_MASK; // make sure the head doesn't overflow
 }
 
 uint8_t cbuffer_get(cbuffer_t* buf) {
-    return 0;
+    if (cbuffer_empty(buf) == true)
+    {
+        return NULL;
+    }
+    uint8_t item = buf->buffer[buf->tail];
+    buf->tail = (buf->tail + 1) & BUFFER_MASK;
+    return item;
 }
 
 
 bool cbuffer_full(cbuffer_t* buf) {
-    return false;
+    return ((buf->head - buf->tail) & BUFFER_MASK) == BUFFER_MASK;;
 }
 
 
-void cbuffer_clear(cbuffer_t* buf) {}
+void cbuffer_clear(cbuffer_t* buf) {
+    buf->head = 0;
+    buf->tail = 0;
+}
--- test/test_circular_buffer.c
+++ test/test_circular_buffer.c
@@ -1,4 +1,5 @@
 #include <unity.h>
+#include <stdio.h>
 #include "cbuffer.h"
 
 cbuffer_t buff;
@@ -14,9 +15,52 @@ void tearDown(void) {
 void test_circular_buffer_empty_after_init() {
     TEST_ASSERT_TRUE(cbuffer_empty(&buff));
 }
+void test_circular_buffer_add_items() {
+    cbuffer_add(&buff, 1);
+    TEST_ASSERT_FALSE(cbuffer_empty(&buff));
+    cbuffer_add(&buff, 2);
+    TEST_ASSERT_FALSE(cbuffer_empty(&buff));
+    TEST_ASSERT_TRUE(cbuffer_get(&buff) == 1);
+    TEST_ASSERT_TRUE(cbuffer_get(&buff) == 2);
+}
+void test_circular_buffer_full() {
+    //test to add items to a full buffer and see if it full
+    //no add items with for loop from 0 to the buffer size
+    for (int i = 0; i < BUFFER_SIZE-1; i++) {
+        cbuffer_add(&buff, i);
+    }
+    TEST_ASSERT_TRUE(cbuffer_full(&buff));
+
+}
+void test_circular_buffer_get_items_after_full() {
+    
+    for (int i = 0; i < BUFFER_SIZE-1; i++) {
+        cbuffer_add(&buff, i);
+    }
+    TEST_ASSERT_TRUE(cbuffer_full(&buff));
+
+    for (int i = 0; i < BUFFER_SIZE-1; ++i)
+    {
+        TEST_ASSERT_TRUE(cbuffer_get(&buff) == i);
+    }
+    TEST_ASSERT_TRUE(cbuffer_empty(&buff));
+}
+void test_circular_buffer_clear() {
+cbuffer_add(&buff, 1);
+cbuffer_add(&buff, 2);
+cbuffer_clear(&buff);
+TEST_ASSERT_TRUE(cbuffer_empty(&buff));
+TEST_ASSERT_FALSE(cbuffer_get(&buff) == 1);
+TEST_ASSERT_FALSE(cbuffer_get(&buff) == 2);
+TEST_ASSERT_TRUE(cbuffer_get(&buff) == NULL); 
+}
 
 void main(void) {
     UNITY_BEGIN();
-    test_circular_buffer_empty_after_init();
+    RUN_TEST(test_circular_buffer_empty_after_init);
+    RUN_TEST(test_circular_buffer_add_items);
+    RUN_TEST(test_circular_buffer_full);
+    RUN_TEST(test_circular_buffer_get_items_after_full);
+    RUN_TEST(test_circular_buffer_clear);
     UNITY_END();
 }
